50866c8b12e451bc863b9a1c522ca79841bd0a8a
FireFly <christoph1.haubrich@arcor.de>
2026-08-01 19:09:02 +0200

    internal redesign, add support for dynamic column width of VDR 2.8.2+

--- displayinfo.c
+++ displayinfo.c
@@ -18,9 +18,10 @@ int AutoCloseSec = 20;
 
 // --- cMenuSystemInfo ----------------------------------------------------
 cMenuSystemInfo::cMenuSystemInfo(const char *Script)
-:cOsdMenu(tr("System Information"), 14, 18)
+:cOsdMenu(tr("System Information"))
 {
    InfoLines = NULL;
+   firstDisplay = true;
    autocloseTimer.Set(AutoCloseSec * 1000);
 
    if (access(Script, X_OK)) {
@@ -30,7 +31,7 @@ cMenuSystemInfo::cMenuSystemInfo(const char *Script)
       Add(new cOsdItem(tr("please wait"), osUnknown, false));
 
       InfoLines = new cInfoLines(Script);
-      InfoLines->StateChanged(infolinesState);
+      InfoLines->StateChanged(infolinesState); // init state
    }
    SetHelp(NULL, NULL, NULL, NULL);
 }
@@ -62,40 +63,69 @@ eOSState cMenuSystemInfo::ProcessKey(eKeys Key)
 
 void cMenuSystemInfo::Set()
 {
-   int current = Current();
-   cThreadLock InfoLinesLock(InfoLines);
-
-   if (InfoLines->FirstDisplay())
+   if (firstDisplay)
    {
       Clear();
+#if defined(APIVERSNUM) && APIVERSNUM >= 30012
+      int t1 = 0;
+      int t2 = 0;
+      const cFont *Font = dynamic_cast<cSkinDisplayMenu *>(cSkinDisplay::Current())->GetTextAreaFont(false);
+#endif
+      cThreadLock InfoLinesLock(InfoLines);
       for (cInfoLine *line = InfoLines->First(); line; line = InfoLines->Next(line))
       {
-         line->SetOsdItem(new cOsdItem((const char *)line->GetStr(), osUnknown, true));
          Add(line->GetOsdItem());
+#if defined(APIVERSNUM) && APIVERSNUM >= 30012
+         const char *text = line->GetStr();
+         if (Font && !isempty(text)) {
+            if (const char *Tab1 = strchr(text, '\t')) {
+               int l = Font->Width(cString(text, Tab1));
+               if (l > t1)
+                  t1 = l;
+               if (const char *Tab2 = strchr(Tab1 + 1, '\t')) {
+                  int l = Font->Width(cString(Tab1 + 1, Tab2));
+                  if (l > t2)
+                     t2 = l;
+               }
+            }
+         }
+#endif
       }
-      SetCurrent(Get(current));
-      InfoLines->SetOsdInitialized();
-      Display();
+
+#if defined(APIVERSNUM) && APIVERSNUM >= 30012
+      if (Font) {
+         if (t1 > 0) {
+            t1 += Font->Width("  "); // to have some distance between name and value
+            if (Font->Width("M") > 1)
+               t1 = -t1;
+         }
+         if (t2 > 0) {
+            t2 += Font->Width("  "); // to have some distance between name and value
+            if (Font->Width("M") > 1)
+               t2 = -t2;
+         }
+      }
+      SetCols(t1, t2);
+#else
+      SetCols(14, 18);
+#endif
+      firstDisplay = false;
    }
    else
    {
+      cThreadLock InfoLinesLock(InfoLines);
       for (cInfoLine *line = InfoLines->First(); line; line = InfoLines->Next(line))
-      {
-         line->GetOsdItem()->SetText((const char *)line->GetStr());
-      }
-      Display();
+         line->UpdateOsdText();
    }
+   Display();
 }
 
 
 // --- cInfoLines ----------------------------------------------------
-
 cInfoLines::cInfoLines(const char *Script)
-:cThread("systeminfo")
+:cThread("systeminfo", true)
 {
    state = 0;
-   firstDisplay = true;
-   OsdInitialized = false;
    scriptname = Script;
    Start();
 }
@@ -113,10 +143,10 @@ cInfoLines::~cInfoLines()
 bool cInfoLines::StateChanged(int &State)
 {
    int NewState = state;
-   bool Result = State != NewState;
-   State = state;
+   bool changed = State != NewState;
+   State = NewState;
 
-   return Result;
+   return changed;
 }
 
 
@@ -156,64 +186,54 @@ float cInfoLines::GetCpuPct(void)
 cString cInfoLines::PrepareInfoline(int Line, bool *IsStatic)
 {
    #define BARLEN 30
-   char progressbar[BARLEN+3] = "";
+   char progressbar[BARLEN+3];
    progressbar[0] = '[';
    progressbar[BARLEN+1] = ']';
    progressbar[BARLEN+2] = 0;
+   cString osdline;
 
-   char *systeminfo = NULL;
-   cString osdline = NULL;
-
-   systeminfo = ExecShellCmd((const char *)cString::sprintf("%s %d", *scriptname, Line));
-   //isyslog("systeminfo: %2d, %s", Line, systeminfo);
-   if (NULL != (const char *)systeminfo) {
+   cString systeminfo = ExecShellCmd(*cString::sprintf("%s %d", *scriptname, Line));
+   //isyslog("systeminfo:  %2d, %s", Line, *systeminfo);
+   if (!isempty(*systeminfo)) {
       float fval1, fval2 = 0;
       char *pname = NULL;
-      *IsStatic = !strncasecmp(systeminfo, "s\t", 2);
-      osdline = *IsStatic ? systeminfo+2 : systeminfo;
-
-      // check for unformatted disk usage (in kB without units)
-      if (3 == sscanf(systeminfo, "%m[a-zA-Z,/0-9 ]: %f %f", &pname, &fval1, &fval2)) {
-         fval1 /= 1024.0*1024.0;
-         fval2 /= 1024.0*1024.0;
-         if (fval1 == 0.0)
-            osdline = cString::sprintf("%s:\t%.1f GB / %.1f GB", pname, fval1, fval2);
-         else {
-            int frac = min(BARLEN,max(0, int((fval1-fval2)*BARLEN/fval1)));
-            memset(progressbar + 1,'|',frac);
-            memset(progressbar + 1 + frac ,' ', BARLEN - frac);
-
-            osdline = cString::sprintf("%s:\t%.1f GB / %.1f GB\t%s", pname, fval1, fval2, progressbar);
-         }
-
-         free(pname);
+      if (IsStatic) {
+         *IsStatic = !strncasecmp(*systeminfo, "s\t", 2);
+         systeminfo = *IsStatic ? cString(strdup((*systeminfo) + 2), true) : systeminfo;
       }
 
-      // check for values in kB
-      else if (3 == sscanf(systeminfo, "%m[a-zA-Z,/0-9 ]: %fkB %fkB", &pname, &fval1, &fval2)) {
+      // check for two values (total and free) with our without 'kB' like e.g. disk usage
+      if (3 == sscanf(*systeminfo, "%m[a-zA-Z,/0-9 ]: %f %f", &pname, &fval1, &fval2) ||
+         (3 == sscanf(*systeminfo, "%m[a-zA-Z,/0-9 ]: %f kB %f kB", &pname, &fval1, &fval2)))
+      {
+         compactspace(pname);
          if (fval1 == 0.0)
             osdline = cString::sprintf("%s:\t%.1f kB / %.1f kB", pname, fval1, fval2);
          else {
-            int frac = min(BARLEN,max(0, int((fval1-fval2)*BARLEN/fval1)));
+            int frac = min(BARLEN, max(0, int((fval1 - fval2) * BARLEN / fval1)));
             memset(progressbar + 1,'|',frac);
             memset(progressbar + 1 + frac ,' ', BARLEN - frac);
 
-            if (fval1 > 1024.0*1024.0 && fval2 > 1024.0*1024.0) {
-               fval1 /=1024.0*1024.0;
-               fval2 /=1024.0*1024.0;
-               osdline = cString::sprintf("%s:\t%.1f GB / %.1f GB\t%s", pname, fval1, fval2, progressbar);
-            } else if (fval1 > 1024.0 && fval2 > 1024.0) {
-               fval1 /=1024.0;
-               fval2 /=1024.0;
-               osdline = cString::sprintf("%s:\t%.1f MB / %.1f MB\t%s", pname, fval1, fval2, progressbar);
-            } else 
-               osdline = cString::sprintf("%s:\t%.1f kB / %.1f kB\t%s", pname, fval1, fval2, progressbar);
+            cString unit = "kB";
+            if (fval1 > 1024.0 && fval2 > 1024.0) {
+               fval1 /= 1024.0;
+               fval2 /= 1024.0;
+               unit = "MB";
+               if (fval1 > 1024.0 && fval2 > 1024.0) {
+                  fval1 /= 1024.0;
+                  fval2 /= 1024.0;
+                  unit = "GB";
+               }
+            }
+            osdline = cString::sprintf("%s:\t%.1f %s / %.1f %s\t%s", pname, fval1, *unit, fval2, *unit, progressbar);
+            free(pname);
          }
-         free(pname);
       }
 
       // check for CPU%
-      else if (1 == sscanf(systeminfo, "%m[a-zA-Z,/0-9 ]: CPU%%", &pname) && NULL != strstr(systeminfo, "CPU%")) {
+      else if (strstr(systeminfo, "CPU%") && (1 == sscanf(*systeminfo, "%m[a-zA-Z,/0-9 ]: CPU%%", &pname)))
+      {
+         compactspace(pname);
          fval2 = GetCpuPct();
          int frac = min(BARLEN,max(0, int(fval2*BARLEN/100.0)));
          memset(progressbar + 1,'|',frac);
@@ -222,6 +242,8 @@ cString cInfoLines::PrepareInfoline(int Line, bool *IsStatic)
          osdline = cString::sprintf("%s:\t%.1f %%\t%s", pname, fval2, progressbar);
          free(pname);
       }
+      else
+         osdline = systeminfo;
    }
    return osdline;
 }
@@ -230,80 +252,51 @@ cString cInfoLines::PrepareInfoline(int Line, bool *IsStatic)
 void cInfoLines::Action()
 {
    int line = 0;
-   cThread::Lock();
-   Clear();
-   cThread::Unlock();
-   cString osdline = NULL;
-
-   GetCpuPct(); // init cpu usage
+   cString osdline;
+   GetCpuPct(); // init CPU usage
 
    do {
       bool isStatic = false;
 
       osdline = PrepareInfoline(++line, &isStatic);
-      if ((const char*)osdline) {
-         cThread::Lock();
+      if (!isempty(*osdline)) {
          Add(new cInfoLine(osdline, isStatic));
-         cThread::Unlock();
       }
    }
-   while (Running() && NULL != (const char*)osdline && line <= MAX_LINES);
+   while (Running() && !isempty(*osdline) && line <= MAX_LINES);
 
-   if (!First()) {
-      cThread::Lock();
-      osdline = tr("Error getting system information");
-      Add(new cInfoLine(osdline, true));
+   if (First() == NULL) {
+      Add(new cInfoLine(tr("Error getting system information"), true));
       state++;
-      cThread::Unlock();
    }
-      else
+   else
    {
-      cThread::Lock();
       state++;
-      cThread::Unlock();
-      if (Running())
-         Wait.Wait(RefreshIntervall*1000);
 
       while (Running()) {
-         cInfoLine * currentline = NULL;
-         cThread::Lock();
-         if (OsdInitialized)
-            firstDisplay = false;
-         currentline = First();
-         cThread::Unlock();
-
+         Wait.Wait(RefreshIntervall*1000);
+         cInfoLine *currentline = First();
          line = 0;
-         do {
-            cString osdline = NULL;
-            bool isStatic = false;
-            line++;
-            if (!currentline || !currentline->isStatic()) {
-               osdline = PrepareInfoline(line, &isStatic);
-               if ((const char*)osdline) {
+         while (Running() && currentline && ++line <= MAX_LINES) {
+            if (!currentline->isStatic()) {
+               osdline = PrepareInfoline(line);
+               if (!isempty(*osdline)) {
                   cThread::Lock();
-                  currentline->SetStr(osdline);
+                  currentline->SetStr(*osdline);
                   cThread::Unlock();
                }
             }
             currentline = Next(currentline);
          }
-         while (Running() && NULL != currentline && line <= MAX_LINES);
 
-         cThread::Lock();
          state++;
-         cThread::Unlock();
-
-         if (Running()) {
-            Wait.Wait(RefreshIntervall*1000);
-         }
       } // while Running()
    }
-   isyslog("systeminfo: Action end");
 }
 
 
-char *cInfoLines::ExecShellCmd(const char *Cmd)
-{  // taken from vdr config.c
+cString cInfoLines::ExecShellCmd(const char *Cmd)
+{  // taken from vdr menu.c with modifications
    char *result = NULL;
    //isyslog("executing command '%s'", Cmd);
    cPipe p;
@@ -313,31 +306,32 @@ char *cInfoLines::ExecShellCmd(const char *Cmd)
       int l = 0;
       int c;
       while ((c = fgetc(p)) != EOF) {
-         if (!l && c == ' ') continue; // skip leading spaces
+         if ((!l || result[l-1]== '\t') && c == 0x20) continue; // skip leading spaces
+         if ( c == 0x0A || c == 0x0D) continue;
 
-         if (l % 20 == 0)
-            result = (char *)realloc(result, l + 21);
+         if (l % 50 == 0) {
+            if (char *NewBuffer = (char *)realloc(result, l + 51))
+               result = NewBuffer;
+            else {
+               esyslog("ERROR: out of memory");
+               break;
+            }
+         }
          result[l++] = c;
       }
       p.Close();
+      if (result) {
+         result[l] = 0;
 
 #ifdef DEBUG
-      isyslog("result: '%s'", result);
-      cString hex;
-      for (int i = 0; i<= l; i++) {
-         hex = cString::sprintf("%s %02X", (const char *)hex, result[i]);
-      }
-      isyslog("result: %s", (const char *)hex);
+         isyslog("result: '%s'", result);
+         cString hex = "";
+         for (int i = 0; i <= l; i++)
+            hex = cString::sprintf("%s %02X", *hex, result[i]);
+         isyslog("resultHex: %s", *hex);
 #endif
-
-      if (result) {
-         if (result[l-1] == 0x0A)
-            result[l-1] = 0;
-         else
-            result[l] = 0;
       }
-      //isyslog("result: '%s'", result);
    }
 
-  return result;
+  return cString(result, true);
 }
--- displayinfo.h
+++ displayinfo.h
@@ -9,6 +9,7 @@
 
 #include <vdr/config.h>
 #include <vdr/osdbase.h>
+#include <atomic>
 
 extern int RefreshIntervall;
 extern int AutoCloseSec;
@@ -17,37 +18,33 @@ class cInfoLine : public cListObject{
 private:
    cString string;
    bool isstatic;
-   cOsdItem *osditem;
+   cOsdItem *osdItem;
 public:
-   cInfoLine(cString String, bool IsStatic) { SetStr(String); isstatic = IsStatic; } ;
+   cInfoLine(cString String, bool IsStatic) { osdItem = new cOsdItem(String); string = String; isstatic = IsStatic; } ;
    ~cInfoLine() {};
-   void SetStr(cString String) { string = String; };
-   cString GetStr(void) { return string; };
-   void SetOsdItem(cOsdItem *OsdItem) { osditem = OsdItem; }
-   cOsdItem *GetOsdItem(void) { return osditem; }
+   void SetStr(const char *String) { string = String; };
+   const char *GetStr(void) const { return *string; };
    bool isStatic(void) { return isstatic; }
+   cOsdItem *GetOsdItem(void) { return osdItem; }
+   void UpdateOsdText(void) { osdItem->SetText(string); };
 };
 
 
 class cInfoLines : public cList<cInfoLine>, public cThread {
 private:
-   int state;
+   std::atomic<int> state;
    cString scriptname;
    cCondWait Wait;
-   bool firstDisplay;
-   bool OsdInitialized;
    unsigned long long ticks[4], ticksold[4];
 
    void Action();
    float GetCpuPct();
-   char *ExecShellCmd(const char *Cmd);
-   cString PrepareInfoline(int Line, bool *IsStatic);
+   cString ExecShellCmd(const char *Cmd);
+   cString PrepareInfoline(int Line, bool *IsStatic = NULL);
 public:
    cInfoLines(const char *Script);
    ~cInfoLines();
    bool StateChanged(int &State);
-   bool FirstDisplay(void) { return firstDisplay; };
-   void SetOsdInitialized() { OsdInitialized = true; };
 };
 
 
@@ -55,9 +52,8 @@ class cMenuSystemInfo : public cOsdMenu {
 private:
    cInfoLines *InfoLines;
    int infolinesState;
+   bool firstDisplay;
    cTimeMs autocloseTimer;
-   char scriptpath[MaxFileName];
-   char *ExecShellCmd(const char *Cmd);
    void Set(void);
 public:
    cMenuSystemInfo(const char *Script);
